942914cfc4a9a3fa799930462ff0e7439b5007f8
Yousof Wakili <ywakili18@gmail.com>
2021-10-11 13:20:25 -0700

    ticket table created

---
+++ models/ticket.js
@@ -0,0 +1,27 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Ticket extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Ticket.init(
+    {
+      ticketTitle: DataTypes.STRING,
+      ticketDescription: DataTypes.STRING,
+      priority: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Ticket',
+      tableName: 'tickets'
+    }
+  )
+  return Ticket
+}
