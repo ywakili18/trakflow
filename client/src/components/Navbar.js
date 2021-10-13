a08a52f1f7051cdb6d832cd099f12340e1f2bbc9
Yousof Wakili <ywakili18@gmail.com>
2021-10-12 22:42:51 -0700

    Re did navigation bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,70 +7,69 @@ const Navbar = ({ authenticated, user, handleLogOut }) => {
   let unauthenticatedOptions
   if (!user) {
     unauthenticatedOptions = (
-      <nav>
-        <NavLink
-          className=" 
-        hover:text-blue-600
-        transition-all mx-auto p-8"
-          to="/"
-        >
-          Login
-        </NavLink>
-        <NavLink
-          className=" 
+      <nav class="text-gray-900 flex  bg-pink-50 navbar justify-between text-2xl">
+        <div class="flex">
+          <NavLink
+            className=" 
+        hover:text-indigo-400
+        transition-all  p-8"
+            to="/"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            className=" 
         hover:text-pink-400
-        transition-all mx-auto p-8 "
-          to="/register"
-        >
-          Register
-        </NavLink>
-        <NavLink
-          className="text-gray-700 hover:text-pink-400 transition-all flex  bg-purple-200"
-          to="/"
-        >
-          <CgPacman className="text-7xl sm:text-8xl" />
-          <button class="text-md sm:text-xl font-bold tracking-widest p-1">
-            TrakFlow
-          </button>
-        </NavLink>
+        transition-all  p-8 "
+            to="/register"
+          >
+            Register
+          </NavLink>
+          <NavLink
+            className=" 
+        
+        hover:text-red-600 
+        transition-all p-8"
+            to="/about"
+          >
+            About
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink
+            className="hover:text-pink-600 transition-all flex text-gray-500"
+            to="/"
+          >
+            <CgPacman className="text-7xl sm:text-8xl" />
+            <button>TrakFlow</button>
+          </NavLink>
+        </div>
       </nav>
     )
   }
 
   const authenticatedOptions = (
-    <nav>
-      <NavLink
-        className=" 
+    <nav class="text-gray-900 flex  bg-pink-50 navbar justify-between text-2xl">
+      <div class="flex">
+        <NavLink
+          className=" 
         hover:text-yellow-600
         transition-all mx-auto p-8 flex "
-        to="/dashboard"
-      >
-        Dashboard
-        <MdOutlineDashboardCustomize className="text-lg sm:text-2xl sm:mt-1" />
-      </NavLink>
-      <NavLink
-        className=" 
+          to="/dashboard"
+        >
+          Dashboard
+          <MdOutlineDashboardCustomize className="text-lg sm:text-2xl sm:mt-1" />
+        </NavLink>
+        <NavLink
+          className=" 
         hover:text-blue-600
         transition-all mx-auto p-8"
-        onClick={handleLogOut}
-        to="/"
-      >
-        Logout
-      </NavLink>
-      <NavLink
-        className="text-gray-700 hover:text-pink-400 transition-all flex  bg-purple-200"
-        to="/Dashboard"
-      >
-        <CgPacman className="text-7xl sm:text-8xl" />
-        <button class="text-md sm:text-xl font-bold tracking-widest p-1">
-          TrakFlow
-        </button>
-      </NavLink>
-    </nav>
-  )
-  return (
-    <div class="text-gray-600  bg-red-100 flex justify-between navbar">
-      <nav class="flex  text-sm sm:text-xl">
+          onClick={handleLogOut}
+          to="/"
+        >
+          Logout
+        </NavLink>
         <NavLink
           className=" 
         
@@ -80,8 +79,22 @@ const Navbar = ({ authenticated, user, handleLogOut }) => {
         >
           About
         </NavLink>
-        {authenticated && user ? authenticatedOptions : unauthenticatedOptions}
-      </nav>
+      </div>
+
+      <div>
+        <NavLink
+          className="hover:text-pink-600 transition-all flex text-gray-500"
+          to="/Dashboard"
+        >
+          <CgPacman className="text-7xl sm:text-8xl" />
+          <button>TrakFlow</button>
+        </NavLink>
+      </div>
+    </nav>
+  )
+  return (
+    <div>
+      {authenticated && user ? authenticatedOptions : unauthenticatedOptions}
     </div>
   )
 }
