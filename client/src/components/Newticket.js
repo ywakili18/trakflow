174c3af800eb3c3dc48e07ed1fa0f9c894148de0
Yousof Wakili <ywakili18@gmail.com>
2021-10-14 21:28:00 -0700

    formatted new ticket component with button toggle

--- client/src/components/Newticket.js
+++ client/src/components/Newticket.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BsPlusCircleFill } from 'react-icons/bs'
+import { FcLeftUp2 } from 'react-icons/fc'
 import Client from '../services/api'
 
 const Newticket = (props) => {
@@ -9,7 +9,7 @@ const Newticket = (props) => {
     priority: 0,
     userId: props.user.id
   })
-
+  const [show, setShow] = useState(true)
   // handle change is temporarily holding the information
   const handleChange = (e) => {
     e.preventDefault()
@@ -33,59 +33,86 @@ const Newticket = (props) => {
   }
 
   return (
-    <form
-      className="text-base w-1/2 mx-auto border-8 rounded-2xl p-5 bg-red-50"
-      onSubmit={(e) => handleSubmit(e)}
-    >
+    <div class="">
       {/* Title */}
-      <div className="text-center bg-purple-400 text-sm  text-gray-600 p-2 border-2">
-        <p className="">Submit a new Ticket</p>
-      </div>
-
-      {/* Ticket Title */}
-      <div className="flex border  titleContainer">
-        <input
-          className="shadow border w-3/4 mx-auto bg-purple-50 h-1/2 mt-5 p-5 text-center text-xl"
-          placeholder="Ticket title"
-          type="text"
-          name="ticketTitle"
-          value={newTicket.ticketTitle}
-          onChange={(e) => handleChange(e)}
-          id="ticketTitle"
-        ></input>
-      </div>
-      {/* New bug form */}
-      <div className="flex p-5 justify-evenly bg-indigo-50">
-        <input
-          className="bg-white  shadow border h-40 w-1/2 mx-auto"
-          placeholder="Describe in detail new bug/issue"
-          name="ticketDescription"
-          value={newTicket.ticketDescription}
-          onChange={(e) => handleChange(e)}
-          id="ticketDescription"
-          type="text"
-        ></input>
-        {/* Priority level */}
-        <div>
-          <p className="mx-auto border-2 bg-white flex">Priority Level</p>
-          <input
-            className="bg-white  shadow border h-40 w-1/2 mx-auto"
-            placeholder="Describe in detail new bug/issue"
-            name="priority"
-            value={newTicket.priority}
-            onChange={(e) => handleChange(e)}
-            id="priority"
-            type="number"
-          ></input>
-        </div>
-        <button
-          type="submit"
-          className="text-blue-400 hover:text-red-400 transition-all text-white px-4 mt-2 ml-1  mx-auto  text-3xl"
-        >
-          <BsPlusCircleFill />
+      <div className="text-center text-xl font-bold  text-pink-800  p-2 border-2 flex justify-evenly">
+        <button onClick={() => setShow(!show)} className="">
+          Toggle
         </button>
+        {show ? <h1>Hello World ! </h1> : null}
       </div>
-    </form>
+      {/* form component */}
+      <form
+        className="text-sm w-1/2 mx-auto "
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        {/* Ticket Title */}
+        <div>
+          <div className="mt-1 flex flex-col ">
+            <span className="text font-bold">Title</span>
+            <input
+              className="focus:ring-indigo-500 
+              focus:border-indigo-500 flex  
+              shadow-xl border
+              sm:text-sm bg-purple-50 p-2"
+              placeholder="Enter new ticket title"
+              type="text"
+              name="ticketTitle"
+              value={newTicket.ticketTitle}
+              onChange={(e) => handleChange(e)}
+              id="ticketTitle"
+            ></input>
+          </div>
+        </div>
+        {/* New bug form */}
+        <div className="flex-col mt-5 ">
+          <div>
+            <span className="text font-bold">Ticket Description</span>
+          </div>
+
+          <div className="flex">
+            <textarea
+              className="focus:ring-indigo-500 
+              focus:border-indigo-500 flex  
+              shadow-xl border
+              sm:text-sm bg-purple-50 p-10 w-screen"
+              placeholder="Describe in detail new bug/issue"
+              name="ticketDescription"
+              value={newTicket.ticketDescription}
+              onChange={(e) => handleChange(e)}
+              id="ticketDescription"
+              type="text"
+            ></textarea>
+          </div>
+
+          {/* Priority level */}
+          <div className="mt-5 flex-col">
+            <p className="text font-bold mt-1">Priority Level:</p>
+            <input
+              className="bg-white  shadow border  mx-auto"
+              placeholder="Describe in detail new bug/issue"
+              name="priority"
+              value={newTicket.priority}
+              onChange={(e) => handleChange(e)}
+              id="priority"
+              type="number"
+            ></input>
+            <button
+              type="submit"
+              className="mt-4 text-indigo-900 
+            transition-colors 
+            duration-150 
+            bg-purple-100 rounded-lg 
+            focus:shadow-outline 
+            hover:bg-pink-800 py-2 px-4 rounded-lg flex"
+            >
+              <p>Submit ticket</p>
+              <FcLeftUp2 className="text-lg" />
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
