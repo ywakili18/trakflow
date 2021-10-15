6854e33f03a501ccc4ab4b959a818313afec2bbd
Yousof Wakili <ywakili18@gmail.com>
2021-10-15 07:22:36 -0700

    changes

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: 'trakflow_development',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'trakflow_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+  }
+}
