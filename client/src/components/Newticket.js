014e8d0af9121d44bc5fd8c5413d35737f52cd96
Yousof Wakili <ywakili18@gmail.com>
2021-10-14 23:16:26 -0700

    added spring effect for currenttickets

--- client/src/components/Newticket.js
+++ client/src/components/Newticket.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { FcLeftUp2 } from 'react-icons/fc'
 import { HiOutlineTicket } from 'react-icons/hi'
 import Client from '../services/api'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 const Newticket = (props) => {
   const [newTicket, setNewTicket] = useState({
     ticketTitle: '',
@@ -49,82 +49,82 @@ const Newticket = (props) => {
           Submit a new ticket request
           <HiOutlineTicket className="text-xl mt-1 ml-2" />
         </button>
-        {show ? (
-          <motion.form
-            animate={{ rotate: 180 }}
-            transition={{ from: 90, duration: 2 }}
-            className="text-sm w-1/2 mx-auto border-2 p-10 text-left"
-            onSubmit={(e) => handleSubmit(e)}
-          >
-            {/* Ticket Title */}
-            <div>
-              <div className="mt-1 flex flex-col text-blue-500 ">
-                <span className="text font-bold">Title</span>
-                <input
-                  className="focus:ring-indigo-500 
+        <AnimatePresence>
+          {show ? (
+            <motion.form
+              className="text-sm w-1/2 mx-auto border-2 p-10 text-left"
+              onSubmit={(e) => handleSubmit(e)}
+            >
+              {/* Ticket Title */}
+              <div>
+                <div className="mt-1 flex flex-col text-blue-500 ">
+                  <span className="text font-bold">Title</span>
+                  <input
+                    className="focus:ring-indigo-500 
               focus:border-indigo-500 flex  
               shadow-xl border
               sm:text-sm bg-purple-50 p-2"
-                  placeholder="Enter new ticket title"
-                  type="text"
-                  name="ticketTitle"
-                  value={newTicket.ticketTitle}
-                  onChange={(e) => handleChange(e)}
-                  id="ticketTitle"
-                ></input>
+                    placeholder="Enter new ticket title"
+                    type="text"
+                    name="ticketTitle"
+                    value={newTicket.ticketTitle}
+                    onChange={(e) => handleChange(e)}
+                    id="ticketTitle"
+                  ></input>
+                </div>
               </div>
-            </div>
-            {/* New bug form */}
-            <div className="flex-col mt-5 text-blue-500">
-              <div>
-                <span className="text font-bold">Ticket Description</span>
-              </div>
+              {/* New bug form */}
+              <div className="flex-col mt-5 text-blue-500">
+                <div>
+                  <span className="text font-bold">Ticket Description</span>
+                </div>
 
-              <div className="flex">
-                <textarea
-                  className="focus:ring-indigo-500 
+                <div className="flex">
+                  <textarea
+                    className="focus:ring-indigo-500 
               focus:border-indigo-500 flex  
               shadow-xl border
               sm:text-sm bg-purple-50 p-10 w-screen"
-                  placeholder="Describe in detail new bug/issue"
-                  name="ticketDescription"
-                  value={newTicket.ticketDescription}
-                  onChange={(e) => handleChange(e)}
-                  id="ticketDescription"
-                  type="text"
-                ></textarea>
-              </div>
+                    placeholder="Describe in detail new bug/issue"
+                    name="ticketDescription"
+                    value={newTicket.ticketDescription}
+                    onChange={(e) => handleChange(e)}
+                    id="ticketDescription"
+                    type="text"
+                  ></textarea>
+                </div>
 
-              {/* Priority level */}
-              <div className="mt-5 flex-col">
-                <p className="flex justify-start font-bold mt-1">
-                  Priority Level:
-                </p>
-                <input
-                  className="bg-white flex shadow border mt-2"
-                  placeholder="Describe in detail new bug/issue"
-                  name="priority"
-                  value={newTicket.priority}
-                  onChange={(e) => handleChange(e)}
-                  id="priority"
-                  type="number"
-                ></input>
-                <button
-                  type="submit"
-                  className="mt-4 text-indigo-900 
+                {/* Priority level */}
+                <div className="mt-5 flex-col">
+                  <p className="flex justify-start font-bold mt-1">
+                    Priority Level:
+                  </p>
+                  <input
+                    className="bg-white flex shadow border mt-2"
+                    placeholder="Describe in detail new bug/issue"
+                    name="priority"
+                    value={newTicket.priority}
+                    onChange={(e) => handleChange(e)}
+                    id="priority"
+                    type="number"
+                  ></input>
+                  <button
+                    type="submit"
+                    className="mt-4 text-indigo-900 
             transition-colors 
             duration-150 
             bg-purple-100 rounded-lg 
             focus:shadow-outline 
             hover:bg-pink-800 py-2 px-4 rounded-lg flex"
-                >
-                  <p>Submit ticket</p>
-                  <FcLeftUp2 className="text-lg" />
-                </button>
+                  >
+                    <p>Submit ticket</p>
+                    <FcLeftUp2 className="text-lg" />
+                  </button>
+                </div>
               </div>
-            </div>
-          </motion.form>
-        ) : null}
+            </motion.form>
+          ) : null}
+        </AnimatePresence>
       </div>
     </div>
   )
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import Newticket from '../components/Newticket'
 import Currenttickets from '../components/Currenttickets'
+import { motion, AnimatePresence } from 'framer-motion'
 const Dashboard = (props) => {
   return (
-    <div className="">
-      {/* <header class="border h-screen w-2/5 text-2xl text-center">
+    <AnimatePresence>
+      <motion.div className="">
+        {/* <header class="border h-screen w-2/5 text-2xl text-center">
         Dashboard
       </header> */}
-      <Newticket {...props} />
-      <div className="bg-pink-100">
-        <Currenttickets {...props} />
-      </div>
-    </div>
+        <Newticket {...props} />
+        <motion.div
+          initial={{ y: 300, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -300, opacity: 0 }}
+          transition={{ type: 'spring', duration: 0.8 }}
+          className="bg-pink-100"
+        >
+          <Currenttickets {...props} />
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
