d964d3c7b6c587ba9b2ff010f658f07e941fe566
Yousof Wakili <ywakili18@gmail.com>
2021-10-15 21:48:53 -0700

    About page done

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,59 +6,85 @@ const About = () => {
   return (
     <AnimatePresence>
       <motion.div
-        initial={{ x: -300 }}
-        animate={{ x: 0 }}
-        exit={{ x: 300 }}
+        initial={{ y: 300 }}
+        animate={{ y: 0 }}
+        exit={{ y: -300 }}
         transition={{ type: 'tween' }}
-        className="text-blue-900 p-20 flex-col text-center"
+        className="text-gray-700 p-20 flex-col text-center mt-40 "
       >
-        <p class="text-2xl sm:text-3xl mt-20">Social Media Links:</p>
-        <div className="flex flex-col border-4 mt-20 mx-auto">
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://linkedin.com/in/youseffect"
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ type: 'tween', delay: 0.5 }}
+          className="flex-col"
+        >
+          {/* Title */}
+          <p
+            class="
+          text-sm sm:text-3xl text-center
+           mx-auto 
+          font-bold"
           >
-            <button
-              class="
-          text-3xl sm:text-4xl 
-              hover:text-purple-600 
-              focus:ring  transition-all"
+            Check out the repo, follow me on LinkedIn and
+            <span
+              className="text-transparent bg-clip-text mt-10
+            bg-gradient-to-r from-blue-300 to-pink-500 ml-3 font-bold"
             >
-              <FiLinkedin className="ml-4 mt-2" />
-            </button>
-          </a>
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://github.com/ywakili18/trakflow"
-          >
-            <button
-              class="
-          text-3xl sm:text-4xl 
-              hover:text-purple-600 
-              focus:ring  transition-all"
+              site
+            </span>{' '}
+            below!
+          </p>
+          {/* Icons container */}
+          <div class="flex justify-evenly mx-auto">
+            {/* Linkedin */}
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://linkedin.com/in/youseffect"
             >
-              <FiGithub className="ml-4 mt-2" />
-            </button>
-          </a>
-
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://www.linkedin.com/in/youseffect/"
-          >
-            <button
-              class="
-              hover:text-purple-600 
-              focus:ring  transition-all"
+              <button
+                class="
+                text-4xl sm:text-6xl 
+                hover:text-purple-600 
+                focus:ring  transition-all p-10"
+              >
+                <FiLinkedin />
+              </button>
+            </a>
+            {/* Github */}
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://github.com/ywakili18/trakflow"
             >
-              <p className="underline text-3xl sm:text-4xl mt-5">
-                youseffect.com{''}
-              </p>
-            </button>
-          </a>
-        </div>
+              <button
+                class="
+          text-4xl sm:text-6xl 
+              hover:text-purple-600 
+              focus:ring  transition-all p-10"
+              >
+                <FiGithub />
+              </button>
+            </a>
+            {/* Personal site */}
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.youseffect.com/"
+            >
+              <button
+                class="
+              hover:text-purple-600 
+              focus:ring transition-all 
+              underline text-xl sm:text-4xl 
+              p-4 mt-7"
+              >
+                youseffect.com
+              </button>
+            </a>
+          </div>
+        </motion.div>
       </motion.div>
     </AnimatePresence>
   )
