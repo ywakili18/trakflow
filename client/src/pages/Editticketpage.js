83792e3b5c0a116de9e1e06b3afc9aff31f509e1
Yousof Wakili <ywakili18@gmail.com>
2021-10-16 18:38:46 -0700

    update field done and responsive for tickets

--- client/src/pages/Editticketpage.js
+++ client/src/pages/Editticketpage.js
@@ -20,7 +20,6 @@ const Editticketpage = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     Client.put(`/tickets/${updatedTicket.ticketId}`, {
-      ticketTitle: updatedTicket.ticketTitle,
       ticketDescription: updatedTicket.ticketDescription,
       priority: updatedTicket.priority,
       ticketId: updatedTicket.ticketId
@@ -31,48 +30,72 @@ const Editticketpage = (props) => {
   }
   return (
     <div>
-      <div className="flex min-h-screen">
-        {/* image container */}
-        <div className="login w-1/2 "></div>
-        {/* Login form container */}
-        <div className="w-1/2 bg-pink-100 text-gray-800  px-4 py-6 flex flex-col justify-center">
-          <div className="mx-auto text-center">
-            <p className="text-2xl font-light">Edit ticket</p>
-            <div className=" mt-4 bg-white shadow-2xl sm:rounded-lg text-left text-sm">
-              <form className="py-6 px-8" onSubmit={handleSubmit}>
-                <label className="block font-semibold">Ticket Title</label>
-                <input
-                  name="ticketTitle"
-                  value={updatedTicket.ticketTitle}
-                  onChange={(e) => handleChange(e)}
-                  id="ticketTitle"
-                  type="text"
-                  className=" border h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md"
-                />
-                <label className="block mt-3 font-semibold">Edit Ticket</label>
-                <input
-                  name="ticketDescription"
-                  value={updatedTicket.ticketDescription}
-                  onChange={(e) => handleChange(e)}
-                  id="ticketDescription"
-                  type="text"
-                  className=" border w-full h-5 px-2 py-4 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md"
-                />
-                <div className="flex justify-between items-baseline">
-                  <button
-                    className="mt-4 text-indigo-100 
+      {/* image container */}
+      {/* Login form container */}
+      <div className="h-screen bg-pink-100 text-gray-800 flex flex-col justify-center">
+        <div className="mx-auto text-center">
+          <p className="text-xl text-purple-500 font-bold">
+            Trak your new update
+          </p>
+          <div
+            className=" 
+          mt-4 shadow-2xl
+          bg-blue-100 
+          sm:rounded-lg 
+          text-left 
+          text-sm"
+          >
+            <form className="p-20 sm:p-40" onSubmit={handleSubmit}>
+              <label className="font-semibold">Update Ticket</label>
+              <textarea
+                name="ticketDescription"
+                value={updatedTicket.ticketDescription}
+                onChange={(e) => handleChange(e)}
+                id="ticketDescription"
+                type="text"
+                placeholder="What's the new update?"
+                className=" 
+                w-full bg-blue-50
+                border py-10 px-16 mt-5
+                hover:outline-none focus:outline-none 
+                 focus:ring-1 focus:ring-indigo-600 rounded-md"
+              />
+              <div className=" mt-10 flex justify-between">
+                <button
+                  className="text-indigo-100 
                       transition-colors 
                       duration-150 
-                      bg-blue-500 rounded-lg 
+                      bg-blue-100 rounded-lg 
                       focus:shadow-outline 
-                      hover:bg-pink-500 p-5 mx-auto rounded-lg"
-                  >
-                    Save
-                  </button>
-                  <p className="text-sm hover:underline p-4"></p>
-                </div>
-              </form>
-            </div>
+                      hover:bg-black 
+                      hover:text-red-200 text-xl
+                      text-purple-600 font-bold
+                      bg-blue-200
+                      p-5  
+                      rounded-lg"
+                >
+                  Submit
+                </button>
+                <button
+                  onClick={function handleClick(e) {
+                    history.push('/dashboard')
+                  }}
+                  className="text-indigo-100 
+                  transition-colors 
+                  duration-150 
+                  bg-blue-100 rounded-lg 
+                  focus:shadow-outline 
+                  hover:bg-black 
+                  hover:text-red-200 text-xl
+                  text-purple-600 font-bold
+                  bg-blue-200
+                  p-5  
+                  rounded-lg"
+                >
+                  Cancel
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
