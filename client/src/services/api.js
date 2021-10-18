c2cbfc8144eb58349c59caad0f0ada820e3d9788
Yousof Wakili <ywakili18@gmail.com>
2021-10-17 21:58:20 -0700

    test

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://immense-dawn-73702.herokuapp.com/'
+export const BASE_URL = 'http://localhost:3001/api'
 const Client = Axios.create({ baseURL: BASE_URL })
 
 // Intercepts every request axios makes
