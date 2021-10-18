bd249049736724f1c055b9be27b6c5c53c78ef0f
Yousof Wakili <ywakili18@gmail.com>
2021-10-18 07:32:40 -0700

    test

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -24,7 +24,7 @@ export const SignOutUser = async (data) => {
 export const RegisterUser = async (data) => {
   try {
     const res = await Client.post('/auth/register', data)
-    return res.data
+    //
   } catch (error) {
     throw error
   }
